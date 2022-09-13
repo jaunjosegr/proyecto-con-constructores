8826fa61bd9b897276223349e73ca77362431c8b
jg602 <jg602@JUANJOS>
2022-09-13 07:50:48 -0600

    Agregar archivos de proyecto.

---
+++ proyecto con constructores/CLSOP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_con_constructores
+{
+    internal class CLSOP
+    {
+        private double n1;
+        private double n2;
+
+        //constructor
+        public CLSOP(double n1, double n2)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+
+        //Encapsulamiento (las variables que se van a utilizar son N1 Y N2) 
+        public double N1{ get => n1; set => n1 = value; }
+        public double N2 { get => n2; set => n2 = value; }
+
+        //constructor vacio
+        public CLSOP ()
+        {
+
+        }
+
+        //Metodo
+        public double DVSN()
+        {
+            return n1/ n2;
+        }
+
+    }
+
+}
---
+++ proyecto con constructores/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace proyecto_con_constructores
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             CLSOP PP = new CLSOP();
+            PP.N1 = double.Parse(textBox1.Text);
+            PP.N2 = double.Parse(textBox2.Text);
+
+            CLSOP OO = new CLSOP(double.Parse(textBox1.Text),
+            double.Parse(textBox2.Text));  textBox3.Text = OO.DVSN().ToString();
+
+        }
+    }
+}
